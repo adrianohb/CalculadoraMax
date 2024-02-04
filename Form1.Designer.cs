aa922bf6a77d4eade0a5b6eae527811ff7044916
Adriano Hoffmann <adriano_hb@hotmail.com>
2024-02-04 09:20:37 -0300

    Calculadora Max

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,189 +50,224 @@
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(23, 478);
+            this.btnLimpar.Font = new System.Drawing.Font("BankGothic Md BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(11, 242);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(91, 77);
+            this.btnLimpar.Size = new System.Drawing.Size(61, 50);
             this.btnLimpar.TabIndex = 0;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
-            this.btnLimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btnZero
             // 
-            this.btnZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnZero.Location = new System.Drawing.Point(131, 478);
+            this.btnZero.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZero.Location = new System.Drawing.Point(83, 242);
+            this.btnZero.Margin = new System.Windows.Forms.Padding(2);
             this.btnZero.Name = "btnZero";
-            this.btnZero.Size = new System.Drawing.Size(91, 77);
+            this.btnZero.Size = new System.Drawing.Size(61, 50);
             this.btnZero.TabIndex = 1;
             this.btnZero.Text = "0";
             this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
             // 
             // btnVirgula
             // 
-            this.btnVirgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVirgula.Location = new System.Drawing.Point(239, 478);
+            this.btnVirgula.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVirgula.Location = new System.Drawing.Point(155, 242);
+            this.btnVirgula.Margin = new System.Windows.Forms.Padding(2);
             this.btnVirgula.Name = "btnVirgula";
-            this.btnVirgula.Size = new System.Drawing.Size(91, 77);
+            this.btnVirgula.Size = new System.Drawing.Size(61, 50);
             this.btnVirgula.TabIndex = 2;
             this.btnVirgula.Text = ",";
             this.btnVirgula.UseVisualStyleBackColor = true;
             // 
             // btnUm
             // 
-            this.btnUm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUm.Location = new System.Drawing.Point(23, 390);
+            this.btnUm.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUm.Location = new System.Drawing.Point(11, 184);
+            this.btnUm.Margin = new System.Windows.Forms.Padding(2);
             this.btnUm.Name = "btnUm";
-            this.btnUm.Size = new System.Drawing.Size(91, 77);
+            this.btnUm.Size = new System.Drawing.Size(61, 50);
             this.btnUm.TabIndex = 3;
             this.btnUm.Text = "1";
             this.btnUm.UseVisualStyleBackColor = true;
+            this.btnUm.Click += new System.EventHandler(this.btnUm_Click);
             // 
             // btnDois
             // 
-            this.btnDois.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDois.Location = new System.Drawing.Point(131, 390);
+            this.btnDois.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDois.Location = new System.Drawing.Point(83, 184);
+            this.btnDois.Margin = new System.Windows.Forms.Padding(2);
             this.btnDois.Name = "btnDois";
-            this.btnDois.Size = new System.Drawing.Size(91, 77);
+            this.btnDois.Size = new System.Drawing.Size(61, 50);
             this.btnDois.TabIndex = 4;
             this.btnDois.Text = "2";
             this.btnDois.UseVisualStyleBackColor = true;
+            this.btnDois.Click += new System.EventHandler(this.btnDois_Click);
             // 
             // btnTres
             // 
-            this.btnTres.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTres.Location = new System.Drawing.Point(239, 390);
+            this.btnTres.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTres.Location = new System.Drawing.Point(155, 184);
+            this.btnTres.Margin = new System.Windows.Forms.Padding(2);
             this.btnTres.Name = "btnTres";
-            this.btnTres.Size = new System.Drawing.Size(91, 77);
+            this.btnTres.Size = new System.Drawing.Size(61, 50);
             this.btnTres.TabIndex = 5;
             this.btnTres.Text = "3";
             this.btnTres.UseVisualStyleBackColor = true;
+            this.btnTres.Click += new System.EventHandler(this.btnTres_Click);
             // 
             // btnQuatro
             // 
-            this.btnQuatro.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnQuatro.Location = new System.Drawing.Point(23, 302);
+            this.btnQuatro.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuatro.Location = new System.Drawing.Point(11, 127);
+            this.btnQuatro.Margin = new System.Windows.Forms.Padding(2);
             this.btnQuatro.Name = "btnQuatro";
-            this.btnQuatro.Size = new System.Drawing.Size(91, 77);
+            this.btnQuatro.Size = new System.Drawing.Size(61, 50);
             this.btnQuatro.TabIndex = 6;
             this.btnQuatro.Text = "4";
             this.btnQuatro.UseVisualStyleBackColor = true;
+            this.btnQuatro.Click += new System.EventHandler(this.btnQuatro_Click);
             // 
             // btnCinco
             // 
-            this.btnCinco.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCinco.Location = new System.Drawing.Point(131, 302);
+            this.btnCinco.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCinco.Location = new System.Drawing.Point(83, 127);
+            this.btnCinco.Margin = new System.Windows.Forms.Padding(2);
             this.btnCinco.Name = "btnCinco";
-            this.btnCinco.Size = new System.Drawing.Size(91, 77);
+            this.btnCinco.Size = new System.Drawing.Size(61, 50);
             this.btnCinco.TabIndex = 7;
             this.btnCinco.Text = "5";
             this.btnCinco.UseVisualStyleBackColor = true;
+            this.btnCinco.Click += new System.EventHandler(this.btnCinco_Click);
             // 
             // btnSete
             // 
-            this.btnSete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSete.Location = new System.Drawing.Point(23, 214);
+            this.btnSete.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSete.Location = new System.Drawing.Point(11, 70);
+            this.btnSete.Margin = new System.Windows.Forms.Padding(2);
             this.btnSete.Name = "btnSete";
-            this.btnSete.Size = new System.Drawing.Size(91, 77);
+            this.btnSete.Size = new System.Drawing.Size(61, 50);
             this.btnSete.TabIndex = 9;
             this.btnSete.Text = "7";
             this.btnSete.UseVisualStyleBackColor = true;
+            this.btnSete.Click += new System.EventHandler(this.btnSete_Click);
             // 
             // btnOito
             // 
-            this.btnOito.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOito.Location = new System.Drawing.Point(131, 214);
+            this.btnOito.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOito.Location = new System.Drawing.Point(83, 70);
+            this.btnOito.Margin = new System.Windows.Forms.Padding(2);
             this.btnOito.Name = "btnOito";
-            this.btnOito.Size = new System.Drawing.Size(91, 77);
+            this.btnOito.Size = new System.Drawing.Size(61, 50);
             this.btnOito.TabIndex = 10;
             this.btnOito.Text = "8";
             this.btnOito.UseVisualStyleBackColor = true;
+            this.btnOito.Click += new System.EventHandler(this.btnOito_Click);
             // 
             // btnSeis
             // 
-            this.btnSeis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSeis.Location = new System.Drawing.Point(239, 302);
+            this.btnSeis.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeis.Location = new System.Drawing.Point(155, 127);
+            this.btnSeis.Margin = new System.Windows.Forms.Padding(2);
             this.btnSeis.Name = "btnSeis";
-            this.btnSeis.Size = new System.Drawing.Size(91, 77);
+            this.btnSeis.Size = new System.Drawing.Size(61, 50);
             this.btnSeis.TabIndex = 8;
             this.btnSeis.Text = "6";
             this.btnSeis.UseVisualStyleBackColor = true;
+            this.btnSeis.Click += new System.EventHandler(this.btnSeis_Click);
             // 
             // btnNove
             // 
-            this.btnNove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNove.Location = new System.Drawing.Point(239, 214);
+            this.btnNove.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNove.Location = new System.Drawing.Point(155, 70);
+            this.btnNove.Margin = new System.Windows.Forms.Padding(2);
             this.btnNove.Name = "btnNove";
-            this.btnNove.Size = new System.Drawing.Size(91, 77);
+            this.btnNove.Size = new System.Drawing.Size(61, 50);
             this.btnNove.TabIndex = 11;
             this.btnNove.Text = "9";
             this.btnNove.UseVisualStyleBackColor = true;
+            this.btnNove.Click += new System.EventHandler(this.btnNove_Click);
             // 
             // txtResultado
             // 
-            this.txtResultado.Location = new System.Drawing.Point(23, 65);
-            this.txtResultado.Multiline = true;
+            this.txtResultado.Font = new System.Drawing.Font("BankGothic Lt BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(11, 30);
+            this.txtResultado.Margin = new System.Windows.Forms.Padding(2);
             this.txtResultado.Name = "txtResultado";
-            this.txtResultado.Size = new System.Drawing.Size(311, 142);
+            this.txtResultado.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtResultado.Size = new System.Drawing.Size(205, 36);
             this.txtResultado.TabIndex = 17;
-            this.txtResultado.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtResultado.Text = "0";
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(347, 65);
+            this.btnCalcular.Font = new System.Drawing.Font("BankGothic Md BT", 5.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(226, 30);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(91, 143);
+            this.btnCalcular.Size = new System.Drawing.Size(61, 36);
             this.btnCalcular.TabIndex = 16;
             this.btnCalcular.Text = "CALCULAR";
             this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // btnSomar
             // 
-            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSomar.Location = new System.Drawing.Point(347, 214);
+            this.btnSomar.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(227, 70);
+            this.btnSomar.Margin = new System.Windows.Forms.Padding(2);
             this.btnSomar.Name = "btnSomar";
-            this.btnSomar.Size = new System.Drawing.Size(91, 77);
+            this.btnSomar.Size = new System.Drawing.Size(61, 50);
             this.btnSomar.TabIndex = 18;
             this.btnSomar.Text = "+";
             this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
             // 
             // btnSubtrair
             // 
-            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubtrair.Location = new System.Drawing.Point(347, 302);
+            this.btnSubtrair.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(227, 127);
+            this.btnSubtrair.Margin = new System.Windows.Forms.Padding(2);
             this.btnSubtrair.Name = "btnSubtrair";
-            this.btnSubtrair.Size = new System.Drawing.Size(91, 77);
+            this.btnSubtrair.Size = new System.Drawing.Size(61, 50);
             this.btnSubtrair.TabIndex = 19;
             this.btnSubtrair.Text = "-";
             this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
             // 
             // btnMultiplicar
             // 
-            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMultiplicar.Location = new System.Drawing.Point(347, 390);
+            this.btnMultiplicar.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(227, 184);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(2);
             this.btnMultiplicar.Name = "btnMultiplicar";
-            this.btnMultiplicar.Size = new System.Drawing.Size(91, 77);
+            this.btnMultiplicar.Size = new System.Drawing.Size(61, 50);
             this.btnMultiplicar.TabIndex = 20;
             this.btnMultiplicar.Text = "x";
             this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
             // 
             // btnDividir
             // 
-            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDividir.Location = new System.Drawing.Point(347, 478);
+            this.btnDividir.Font = new System.Drawing.Font("BankGothic Md BT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(227, 242);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(2);
             this.btnDividir.Name = "btnDividir";
-            this.btnDividir.Size = new System.Drawing.Size(91, 77);
+            this.btnDividir.Size = new System.Drawing.Size(61, 50);
             this.btnDividir.TabIndex = 21;
             this.btnDividir.Text = "/";
             this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
             // 
             // FormularioCalculadora
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 580);
+            this.ClientSize = new System.Drawing.Size(298, 314);
             this.Controls.Add(this.btnDividir);
             this.Controls.Add(this.btnMultiplicar);
             this.Controls.Add(this.btnSubtrair);
@@ -251,7 +286,10 @@
             this.Controls.Add(this.btnVirgula);
             this.Controls.Add(this.btnZero);
             this.Controls.Add(this.btnLimpar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "FormularioCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "..:Calculadora:..";
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,65 +18,141 @@ namespace Formulario
             InitializeComponent();
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        decimal valor1;
+        decimal valor2;
+        string operacao;
+        decimal resultadoOperacao;
+
+        private void adcionarNumero(int numero)
         {
-            if (checksomar.Checked = true)
+            decimal valor = decimal.Parse(txtResultado.Text);
+            
+            if (valor == 0)
             {
-                checkmultiplicar.Enabled = false;
-                checkdividir.Enabled = false;
-                checksubtrair.Enabled = false;
+                txtResultado.Clear();
+                txtResultado.Text = (txtResultado.Text + numero);
             }
+            else
+                txtResultado.Text = (txtResultado.Text + numero);
         }
 
-        private void checksubtrair_CheckedChanged(object sender, EventArgs e)
+        private void cliqueOperacao(string calculo)
         {
-            if (checksubtrair.Checked = true)
-                {
-                checkmultiplicar.Enabled = false;
-                checksomar.Enabled = false;
-                checkdividir.Enabled = false;
-                }
+            valor1 = decimal.Parse(txtResultado.Text);
+            txtResultado.Clear();
+            txtResultado.Text = ("0");
+            operacao = calculo;
         }
 
-        private void checkmultiplicar_CheckedChanged(object sender, EventArgs e)
+        private void btnUm_Click(object sender, EventArgs e)
         {
-            if (checkmultiplicar.Checked = true)
+            adcionarNumero(1);
+        }
+
+        private void btnDois_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(2);
+           
+        }
+
+        private void btnTres_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(3);
+        }
+
+        private void btnQuatro_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(4);
+        }
+
+        private void btnCinco_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(5);
+        }
+
+        private void btnSeis_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(6);
+        }
+
+        private void btnSete_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(7);
+        }
+
+        private void btnOito_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(8);
+        }
+
+        private void btnNove_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(9);
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            adcionarNumero(0);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "0";
+            operacao = "";
+            valor1 = 0;
+            valor2 = 0;
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            cliqueOperacao("+");
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            cliqueOperacao("-");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            cliqueOperacao("*");
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            cliqueOperacao("/");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (operacao == "+")
             {
-                checksomar.Enabled = false;
-                checksubtrair.Enabled = false;
-                checkdividir.Enabled = false;
+                valor2 = decimal.Parse(txtResultado.Text);
+                resultadoOperacao = (valor1 + valor2);
+                txtResultado.Text = resultadoOperacao.ToString();
             }
-        }
-
-        private void checkdividir_CheckedChanged(object sender, EventArgs e)
-        {
-            if (checkdividir.Checked= true)
-                {
-                   checkmultiplicar.Enabled = false;
-                   checksubtrair.Enabled = false;
-                   checksomar.Enabled = false;
-                }
-        }
-
-        private void btnlimpar_Click(object sender, EventArgs e)
-        {
-            checksomar.Enabled = true;
-            checksomar.Checked = false;
-
-            checksubtrair.Enabled = true;
-            checksubtrair.Checked = false;
-
-            checkdividir.Enabled = true;
-            checkdividir.Checked = false;
-
-            checkmultiplicar.Enabled = true;
-            checkmultiplicar.Checked = false;
+            if (operacao == "-")
+            {
+                valor2 = decimal.Parse(txtResultado.Text);
+                resultadoOperacao = (valor1 - valor2);
+                txtResultado.Text = resultadoOperacao.ToString();
+            }
+            if (operacao == "*")
+            {
+                valor2 = decimal.Parse(txtResultado.Text);
+                resultadoOperacao = (valor1 * valor2);
+                txtResultado.Text = resultadoOperacao.ToString();
+            }
+            if (operacao == "/")
+            { 
+                valor2 = decimal.Parse(txtResultado.Text);
+                resultadoOperacao = (valor1/ valor2);
+                txtResultado.Text = resultadoOperacao.ToString() ;
+            }    
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
